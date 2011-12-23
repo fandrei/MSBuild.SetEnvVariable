ac06a8c2f32bba73530433a96fcfef6232352b86
fandrei <andrei_f_11@mail.ru>
2011-12-23 15:18:54 +0700

    init

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MSBuild.SetEnvVariable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSBuild.SetEnvVariable")]
+[assembly: AssemblyCopyright("Copyright © 2011 Andrei Faber")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fa99053b-c979-4335-b530-3663272b9059")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SetEnvVariable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MSBuild.SetEnvVariable
+{
+	public class SetEnvVariable : Task
+	{
+		private string _name;
+		private string _value;
+
+		[Required]
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		[Required]
+		public string Value
+		{
+			get { return _value; }
+			set { _value = value; }
+		}
+
+		public override bool Execute()
+		{
+			try
+			{
+				Environment.SetEnvironmentVariable(_name, _value);
+				return true;
+			}
+			catch (Exception exc)
+			{
+				Console.WriteLine(exc);
+				return false;
+			}
+		}
+	}
+}
